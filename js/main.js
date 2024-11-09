5cc760d17d45c22c856414c654f11af644ddbb30
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-09 22:16:49 +0200

    js <> sendmessage function added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+
 const send_btn = document.getElementById("btn-primary");
 const input_text = document.getElementById("input-text");
-const chat_container = document.getElementById("chat-container");
+const chat_container = document.getElementById("container");
+
+function sendMessage(messageText) {
+    let messageElement = document.createElement('div');
+    messageElement.classList.add('user-messages');
+    messageElement.innerHTML = '<span>You:</span>' +
+                               '<span>' + messageText + '</span>';
+    chat_container.appendChild(messageElement); 
+    input_text.value = ""; 
+}
+
+send_btn.addEventListener('click', () => {
+    let messageText = input_text.value.trim();
+    if (messageText) {
+        sendMessage(messageText);
+    }
+});
