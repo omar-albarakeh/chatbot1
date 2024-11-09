ffc779014bf0d36818f0a24ec124e9ac361ad17b
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-09 21:58:28 +0200

    updating some stuff

---
+++ js/main.js
@@ -0,0 +1,3 @@
+const send_btn = document.getElementById("btn-primary");
+const input_text = document.getElementById("input-text");
+const chat_container = document.getElementById("chat-container");
