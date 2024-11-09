31d13c5260fd39b3e757909f122147ea413050c3
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-09 22:41:01 +0200

    js <> chatbotmessage function added

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const send_btn = document.getElementById("btn-primary");
 const input_text = document.getElementById("input-text");
 const chat_container = document.getElementById("container");
 
-function sendMessage(messageText) {
+function userMessage(messageText) {
     let messageElement = document.createElement('div');
     messageElement.classList.add('user-messages');
     messageElement.innerHTML = '<span>You:</span>' +
@@ -12,9 +12,22 @@ function sendMessage(messageText) {
     input_text.value = ""; 
 }
 
+
+function chatBotMessage(messageText) {
+    let messageElement = document.createElement('div');
+    messageElement.classList.add('bot-messages');
+    messageElement.innerHTML = '<span>Chat:</span>' +
+                               '<span>' + messageText + '</span>';
+    chat_container.appendChild(messageElement); 
+    input_text.value = ""; 
+}
+
+
+chatBotMessage("hi from chat bot");
+
 send_btn.addEventListener('click', () => {
     let messageText = input_text.value.trim();
     if (messageText) {
-        sendMessage(messageText);
+        userMessage(messageText);
     }
 });
